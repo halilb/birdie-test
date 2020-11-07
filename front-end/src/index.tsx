1a19c29c28ab290ca45992dbcbaa36957cc8f279
hobolo <>
2020-11-07 22:20:01 +0300

    Running prettier over codebase.

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import store from '@App/store';
-import { Provider } from 'react-redux';
-import App from '@App/components/app/App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import store from "@App/store";
+import { Provider } from "react-redux";
+import App from "@App/components/app/App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>,
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
